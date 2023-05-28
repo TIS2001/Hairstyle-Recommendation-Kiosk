497ecdeb886018cfc86c8cc39a1f7022c8b108a5
KINGWUPU <mark980805@gmail.com>
2023-05-29 03:08:17 +0900

    toggle 함수 추가 앞뒤 이동 버튼 추가 작업 남음

--- py_model/frame.py
+++ py_model/frame.py
@@ -17,59 +17,91 @@ class MainUI(tk.Tk):
         self.mainloop()
 
     def clear_frame(self, frame):
-        for widget in  frame.winfo_children():
+        if frame is not None:
+            for widget in frame.winfo_children():
                 widget.destroy()
 
+
     def open_win6(self):
-        # self.img_name = []
         self.win6 = tk.Frame(self, relief="flat", bg="white")
         self.win6.place(x=0, y=0, width=800, height=1280)
+        self.dict1 = {}
+        self.dict2 = {}
+        self.num = 1
+        self.frame1 = tk.Frame(self.win6, bg='#dddddd')
+        self.frame1.place(x=50, y=100)
+        self.frame2 = tk.Frame(self.win6, bg='#dddddd')
+        self.frame2.place(x=50, y=500)
+        self.frame3 = tk.Frame(self.win6, bg='#dddddd')
+        self.frame3.place(x=50, y=800)
+        self.make_btn(self.frame2, [os.path.join("UI/hairstyles_men", f) for f in os.listdir("UI/hairstyles_men") if f.endswith(".png")])
+        self.select_personal()
 
+    def make_btn(self, frame, img_path):
+        self.clear_frame(frame)
+        img_name = []
+        img_size = (150, 150)
+        buttons = []
+        for i in range(4):
+            path = img_path[i]
+            img = Image.open(path)
+            img = img.resize(img_size)
+            photo_img = ImageTk.PhotoImage(img)
+            
+            button = tk.Button(frame, image=photo_img)
+            buttons.append(button)
+            
+            button.configure(command=lambda btn=button: self.toggle_border(btn))
+            button.grid(row=1, column=i, padx=5)
+            
+            self.img_list.append(photo_img)
+            img_name.append(img_path[i].split('/')[-1].split('.')[0])
+            label = tk.Label(frame, text=img_name[i])
+            label.grid(row=2, column=i, padx=5)
+    
+    def toggle_border(self, button):
+        parent_frame = button.winfo_parent()  # button의 부모 프레임을 찾음
+        if parent_frame == str(self.frame2):  # 부모 프레임이 frame2인 경우
+            dict_var = self.dict1
+        elif parent_frame == str(self.frame3):  # 부모 프레임이 frame3인 경우
+            dict_var = self.dict2
+        else:
+            dict_var = {}
         
-        def make_btn(frame, img_path):
-            self.clear_frame(frame)
-            img_name = []
-            img_size = (150, 150)
-            for i in range(4):
-                path = img_path[i]
-                img = Image.open(path)
-                img = img.resize(img_size)
-                photo_img = ImageTk.PhotoImage(img)
-                button = tk.Button(frame, image=photo_img)
-                button.grid(row=1, column=i, padx=5)
-                self.img_list.append(photo_img)
-                img_name.append(img_path[i].split('/')[-1].split('.')[0])
-                label = tk.Label(frame, text=img_name[i])
-                label.grid(row=2, column=i, padx=5)
+        if button.cget("relief") == "solid":
+            button.config(relief="flat", highlightthickness=0)
+            dict_var.clear()
+        else:
+            if bool(dict_var):
+                int_keys = [k for k in dict_var.keys() if isinstance(k, int)]
+                dict_var[int_keys[0]].config(relief="flat", highlightthickness=0)         
+                dict_var.clear()
+            
+            button.config(relief="solid", highlightthickness=2, highlightbackground="red")
+            dict_var[self.num] = button
 
-        def personal_cmd():
-            selected_image = var.get()
-            if selected_image == 1:
-                dir_path="UI/colors/봄웜"
-                print("봄웜")
-            elif selected_image == 2:
-                dir_path="UI/colors/여쿨"
-                print("여쿨")
-            elif selected_image == 3:
-                dir_path="UI/colors/갈웜"
-                print("갈웜")
-            elif selected_image == 4: 
-                dir_path="UI/colors/겨쿨"
-                print("겨쿨")
-            make_btn(frame3, [os.path.join(dir_path, f) for f in os.listdir(dir_path) if f.endswith(".JPG")])
+    def personal_cmd(self):
+        selected_image = self.var.get()
+        if selected_image == 1:
+            dir_path = "UI/colors/봄웜"
+            print("봄웜")
+        elif selected_image == 2:
+            dir_path = "UI/colors/여쿨"
+            print("여쿨")
+        elif selected_image == 3:
+            dir_path = "UI/colors/갈웜"
+            print("갈웜")
+        elif selected_image == 4:
+            dir_path = "UI/colors/겨쿨"
+            print("겨쿨")
+        
+        self.make_btn(self.frame3, [os.path.join(dir_path, f) for f in os.listdir(dir_path) if f.endswith(".JPG")])
+        self.dict2={}
+    
+   
 
 
-        frame1 = tk.Frame(self.win6, bg='#dddddd')
-        frame1.place(x=50, y=100)
-        # make_btn(frame1, [os.path.join("UI/hairstyles", f) for f in os.listdir("UI/hairstyles") if f.endswith(".png")])
-       
-        frame2 = tk.Frame(self.win6, bg='#dddddd')
-        frame2.place(x=50, y=500)
-        make_btn(frame2, [os.path.join("UI/hairstyles_men", f) for f in os.listdir("UI/hairstyles_men") if f.endswith(".png")])
-
-        frame3 = tk.Frame(self.win6, bg='#dddddd')
-        frame3.place(x=50, y=800)
-        # make_btn(frame3, ...)
+    def select_personal(self):
         # 배경 이미지 파일 경로
         backgrounds = ["UI/spring.png", "UI/summer.png", "UI/autumn.png", "UI/winter.png"]
         # 전경 이미지 파일 경로
@@ -84,22 +116,22 @@ class MainUI(tk.Tk):
         
         # 전경 이미지를 윈도우 크기로 조정
         resized_foreground = foreground.resize((150, 150))
-        var=tk.IntVar(value=-1)
+        self.var=tk.IntVar(value=-1)
 
         for i, background_image in enumerate(background_images):
             composite_image = Image.alpha_composite(background_image, resized_foreground)
             # ImageTk 객체 생성
             photo = ImageTk.PhotoImage(composite_image)
             # 합성 이미지를 표시할 라벨 생성
-            label = tk.Label(frame1, image=photo)
+            label = tk.Label(self.frame1, image=photo)
             label.grid(row=3, column=i+2)
             # 라벨을 리스트에 추가
             labels.append(label)
             # ImageTk 객체에 대한 참조 유지
             label.image = photo
             image_name = backgrounds[i].split(".")[0]  # 이미지 파일 이름 (확장자 제외)
-            name_label = tk.Radiobutton(frame1, font=("Arial", 15), text=image_name, variable=var, value=i+1,
-                            command=lambda:personal_cmd())
+            name_label = tk.Radiobutton(self.frame1, font=("Arial", 15), text=image_name, variable=self.var, value=i+1,
+                            command=lambda:self.personal_cmd())
             name_label.grid(row=4, column=i+2)
 
 if __name__ == "__main__":
